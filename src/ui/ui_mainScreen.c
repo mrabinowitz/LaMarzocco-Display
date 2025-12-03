573ce72a88f93dd4b0e5ca5ab4deea447756b18e
Roshan Kattel <45326607+Roshankattel@users.noreply.github.com>
2025-12-03 10:17:47 +0545

    fixed UI issues

--- src/ui/ui_mainScreen.c
+++ src/ui/ui_mainScreen.c
@@ -76,6 +76,7 @@ lv_obj_set_height( ui_Arc2, 150);
 lv_obj_set_x( ui_Arc2, -163 );
 lv_obj_set_y( ui_Arc2, 5 );
 lv_obj_set_align( ui_Arc2, LV_ALIGN_CENTER );
+lv_obj_clear_flag( ui_Arc2, LV_OBJ_FLAG_CLICKABLE );    /// Flags
 lv_arc_set_value(ui_Arc2, 0);
 lv_obj_set_style_arc_rounded(ui_Arc2, false, LV_PART_MAIN| LV_STATE_DEFAULT);
 
@@ -92,6 +93,7 @@ lv_obj_set_height( ui_Arc3, 150);
 lv_obj_set_x( ui_Arc3, 32 );
 lv_obj_set_y( ui_Arc3, 0 );
 lv_obj_set_align( ui_Arc3, LV_ALIGN_CENTER );
+lv_obj_clear_flag( ui_Arc3, LV_OBJ_FLAG_CLICKABLE );    /// Flags
 lv_arc_set_value(ui_Arc3, 0);
 
 lv_obj_set_style_arc_color(ui_Arc3, lv_color_hex(0x4040FF), LV_PART_INDICATOR | LV_STATE_DEFAULT );
@@ -149,7 +151,7 @@ lv_label_set_text(ui_CoffeeTempLabel,"");
 lv_obj_set_style_text_align(ui_CoffeeTempLabel, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN| LV_STATE_DEFAULT);
 
 ui_BoilerTempLabel = lv_label_create(ui_mainScreen);
-lv_obj_set_width( ui_BoilerTempLabel, lv_pct(7));
+lv_obj_set_width( ui_BoilerTempLabel, lv_pct(8));
 lv_obj_set_height( ui_BoilerTempLabel, LV_SIZE_CONTENT);   /// 1
 lv_obj_set_x( ui_BoilerTempLabel, 34 );
 lv_obj_set_y( ui_BoilerTempLabel, 83 );
